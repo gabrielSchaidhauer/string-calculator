f6307114acb5d3d294484085363502c498939c56
Gabriel Schaidhauer <gabriel@Gabriels-MacBook-Pro.local>
2021-01-20 00:55:49 -0300

    Adding instructions in case of tools.jar error

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# export JAVA_HOME=/your/java/path
 ./gradlew build > /dev/null && \
 echo "If you want to exit at any moment press Ctrl + C" && \
 java -jar build/libs/string-calculator-1.0.jar
